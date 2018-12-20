78f17e881a0032033e65875ad4ab46dec9dde202
Kyle Kratt <kylekratt@gmail.com>
2018-12-19 23:05:57 -0500

    Added get api route and link, as well as default routing to avoid 404

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,8 @@
 var Friends = require("../data/friends.js");
 module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(Friends);
+    })
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
         var lowestScore = 100;
